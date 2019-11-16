76122fbbcd4b8f86623723b94e7f7162b583fad1
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-11-16 13:43:02 +0200

    Integrate puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,11 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+max_threads = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+min_threads = ENV.fetch('RAILS_MIN_THREADS') { max_threads }
+threads min_threads, max_threads
 
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-# preload_app!
+n_workers = ENV.fetch('PUMA_N_WORKERS') { 0 }
+workers n_workers
+if n_workers.to_i > 0
+  preload_app!
+end
 
 plugin :tmp_restart
